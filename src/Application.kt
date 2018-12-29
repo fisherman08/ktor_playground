da8b737dfc150ff6e8fe794713fd1c4e743e9677
Y Kaneko <orenoikizama@hotmail.com>
2018-12-30 08:20:56 +0900

    form auth

--- src/Application.kt
+++ src/Application.kt
@@ -7,9 +7,13 @@ import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.sessions.*
 import com.fasterxml.jackson.databind.*
+import io.ktor.auth.*
 import io.ktor.jackson.*
 import io.ktor.features.*
+import io.ktor.html.respondHtml
+import kotlinx.html.*
 import java.io.File
+import java.util.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
 
@@ -18,44 +22,122 @@ fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args
 fun Application.module(testing: Boolean = false) {
     install(Sessions) {
         cookie<MySession>(
-            "LSESSION_FEATURE_SESSION_ID",
-             SessionStorageMemory()
+            "SESSION_FEATURE_SESSION_ID",
+            SessionStorageMemory()
             //directorySessionStorage(File(".sessions"), cached = true)
         ) {
-            //transform(SessionTransportTransformerEncrypt(encryptionKey = "dcfdcedbd956398g".toByteArray(), signKey = "dcfdcedbd956398g".toByteArray()))
             cookie.path = "/" // Specify cookie's path '/' so it can be used in the whole site
         }
 
-        cookie<CookieContainer>("container")
     }
 
-    install(ContentNegotiation) {
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
+    install(Authentication) {
+
+        form(name = "myauth") {
+            userParamName = "email"
+            passwordParamName = "password"
+            challenge = FormAuthChallenge.Redirect {
+                "/login"
+            }
+            skipWhen { call ->
+                val session = call.sessions.get<MySession>() ?: MySession()
+                val user_id = session.user_id
+                (user_id != null)
+            }
+            validate { credentials ->
+                if (credentials.name == "admin" && credentials.password == "admin") {
+                    UserPrincipal(id = 11)
+                } else {
+                    null
+                }
+            }
         }
     }
 
+
     routing {
+        intercept(ApplicationCallPipeline.Setup) {
+            // リクエストがあった時点でセッションを貼る
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session)
+        }
+
         get("/") {
+            // 通常はセッションにユーザーIDがセットされていなくてもOK
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
-        get("/session/increment") {
-            val session = call.sessions.get<MySession>() ?: MySession()
-            call.sessions.set(session.copy(count = session.count + 1))
 
-            val cookies = call.sessions.get<CookieContainer>() ?: CookieContainer()
-            cookies.data["hoge"] = "fuga"
-            call.sessions.set(cookies)
-            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        route("/admin") {
+            // /admin以下はセッションにuser_idがセットされていないとダメ
+            intercept(ApplicationCallPipeline.Features){
+                val session = call.sessions.get<MySession>() ?: MySession()
+                val user_id = session.user_id
+                if(user_id == null){
+                    call.respondRedirect("/login")
+                    finish()
+                }
+            }
+
+            route("index") {
+                    get {
+                        call.respondHtml {
+                            body {
+                                div {
+                                    +"admin!"
+                                }
+                            }
+                        }
+                    }
+
+            }
+
         }
 
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
+
+        route("/login") {
+            get {
+                call.respondHtml {
+                    body {
+                        form(action = "/login", method = FormMethod.post) {
+                            p {
+                                +"Email: "
+                                textInput(name = "email") { value = "" }
+                            }
+                            p {
+                                +"Password: "
+                                passwordInput(name = "password") { value = "" }
+                            }
+                            p {
+                                submitInput { value = "Login" }
+                            }
+                        }
+                    }
+                }
+            }
+
+            authenticate("myauth") {
+                post {
+                    val principal = call.principal<UserPrincipal>()
+                    if (principal == null) {
+                        call.respondRedirect("/login")
+                        return@post
+                    }
+
+                    val session = call.sessions.get<MySession>() ?: MySession()
+                    call.sessions.set(session.copy(user_id = principal!!.id))
+
+                    call.respondRedirect("/admin/index")
+                }
+            }
         }
     }
 }
 
-data class MySession(val count: Int = 0)
-data class CookieContainer(val data: HashMap<String, String> = hashMapOf())
+data class MySession(
+    val count: Int = 0,
+    val updated_on: Long = Calendar.getInstance(TimeZone.getTimeZone("UTC")).timeInMillis,
+    val user_id: Int? = null
+)
 
+data class UserPrincipal(val id: Int) : Principal
